68bdab6c13fd9636b9e19062877c765d77eb15e5
kubaz <zurawikjakub@gmail.com>
2023-05-11 16:15:18 +0200

    Core of Wave System

---
+++ Assets/Scripts/WaveSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    [SerializeField] private int firstWave = 5;
+    [SerializeField] private GameObject respawnPoint;
+}
