74b6a30aa566ddc6932163910ad3825a68bfb3ab
kubaz <zurawikjakub@gmail.com>
2023-05-25 21:16:17 +0200

    Add DOTween & Medicine Prefab
    need to add model

--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
     public int addHealth = 25;
+
+    private void Start()
+    {
+        transform.DOMoveY(1f, 1f).SetLoops(-1, LoopType.Yoyo);
+        transform.DORotate(new Vector3(0, 360, 0), 3, RotateMode.LocalAxisAdd).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        //transform.DOScale(0.5f, 1f).SetLoops(-1, LoopType.Yoyo);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.tag == "Player")
         {
             HealthSystem.currentPlayerHealthStatic += addHealth;
             Destroy(gameObject);
